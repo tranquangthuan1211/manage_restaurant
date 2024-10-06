a9db72c28ec457ba60df51d555467da4b831405c
QuangThuan <tranquanthuan132@gmail.com>
2024-10-06 21:12:50 +0700

    customize/client

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { type Page as PageType } from "../types/page";
+import { Stack } from "@mui/material";
+
+
+const Page: PageType = () => {
+    return (
+        <div>
+            <h1>heheheheh</h1>
+        </div>
+    )
+}
+
+export default Page;
